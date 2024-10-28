2ff6f21777825f0b4eac776de5b8f1ac3a5d7980
Sharpless298 <sharpless298@gmail.com>
2024-10-29 07:43:33 +0800

    upd

--- hw2/MemAdress.c
+++ hw2/MemAdress.c
@@ -2,5 +2,5 @@
 
 int main() {
 	int x;
-	printf("x is allocated at memory address %x-%x.\n", &x, (unsigned long)(&x)+sizeof(x)-1);
+	printf("x is allocated at memory address 0x%lx-0x%lx.\n", &x, (unsigned long)(&x)+sizeof(x)-1);
 }
