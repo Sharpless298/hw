735f6019bc29f6b6e7303941825b68afb0352b61
Sharpless298 <sharpless298@gmail.com>
2024-11-08 00:14:22 +0800

    upd

--- hw4/SquarePyramidalNum.c
+++ hw4/SquarePyramidalNum.c
@@ -3,22 +3,12 @@
 int main() {
 	printf("Please input an integer: ");
 	int n;
+	long long ans = 0;
 	scanf("%d", &n);
-	if(n>20 || n<0) {
-		printf("Input Error");
-		return 0;
-	}
-	if(n==0 || n==1) {
-		printf("%d! = 1\n", n);
-		return 0;
-	}
-	long long ans = 1;
-	printf("%d! = ", n);
 	for(int i=0; i<n; i++) {
-		if(i) printf(" * ");
-		printf("%d", i+1);
-		ans *= i+1;
+		if(i) printf(" + ");
+		printf("%lld", 1LL*(i+1)*(i+1));
+		ans += (i+1)*(i+1);
 	}
-
 	printf(" = %lld", ans);
 }
