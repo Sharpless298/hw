dd4472cbc748668737db72bb5d7d8a7f4f9f052f
Sharpless298 <sharpless298@gmail.com>
2024-11-12 18:16:02 +0800

    upd

--- hw4/IsPrimeNumber.c
+++ hw4/IsPrimeNumber.c
@@ -4,6 +4,11 @@ int main() {
 	printf("Please input an integer: ");
 	int n;
 	scanf("%d", &n);
+	if(n == 1) {
+		printf("The integer %d is not a prime number!\n", n);
+		return 0;
+	}
+
 	for(int i=2; i*i<=n; i++) {
 		if(n%i == 0) {
 			printf("The integer %d is not a prime number!\n", n);
