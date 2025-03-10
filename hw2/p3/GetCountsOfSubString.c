71844e4f63b261bddf534894b4abfd0a71b7397d
Sharpless298 <sharpless298@gmail.com>
2025-03-11 02:02:45 +0800

    upd

--- hw2/p3/GetCountsOfSubString.c
+++ hw2/p3/GetCountsOfSubString.c
@@ -1,18 +1,18 @@
 #include "GetCountsOfSubString.h"
 
 int GetCountsOfSubString(char s[], char c[]) {
-	int id_c = 129;
+	int n = 129;
 	for (int i = 0; i < 129; i++) {
 		if (c[i] == '\0') {
-			id_c = i+1;
+			n = i+1;
 			break;
 		}
 	}
 
 	int cnt = 0;
-	for (int i = 0; i < 4096; i++) {
+	for (int i = 0; i < 4096-n; i++) {
 		int f = 1;
-		for (int j = 0; j < id_c; j++) {
+		for (int j = 0; j < n; j++) {
 			if (s[i+j] != c[j]) {
 				f = 0;
 				break;
